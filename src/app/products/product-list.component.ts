d04b8b89b7b217115cdeadbe3d7f814a9a15e41f
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-10-03 04:34:37 -0700

    onPush, declarative syntax

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,32 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { EMPTY, Observable, of } from 'rxjs';
+import { EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-
-import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products$: Observable<Product[]>;
+  // declarative style
+  products$ = this.productService.products$.pipe(
+    catchError((err) => {
+      // this.errorMessage = err; // onPush will not notice this change
+      return EMPTY;
+    })
+  );
 
   constructor(private productService: ProductService) {}
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts().pipe(
-      catchError((err) => {
-        this.errorMessage = err;
-        return EMPTY;
-      })
-    );
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
