0ae75af1ae6612e394d0b22d004d911b97696ce2
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-10-03 20:43:02 -0700

    product category observable

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { EMPTY } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 import { Product } from './product';
 import { ProductService } from './product.service';
 
@@ -31,17 +32,28 @@ export class ProductListComponent {
   // declarative style
   products$ = this.productService.productsWithCategory$.pipe(
     catchError((err) => {
+      this.errorMessage = err;
       return EMPTY;
     })
   );
 
-  constructor(private productService: ProductService) {}
+  categories$ = this.productCategoryService.productCategories$.pipe(
+    catchError((err) => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
+
+  constructor(
+    private productService: ProductService,
+    private productCategoryService: ProductCategoryService
+  ) {}
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.selectedCategoryId = +categoryId;
   }
 }
